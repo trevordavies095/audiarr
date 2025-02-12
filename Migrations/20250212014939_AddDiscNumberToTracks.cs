492a14a394e42648993b99e35858abdeced37b24
trevordavies095 <me@trevordavies095.com>
2025-02-11 20:54:33 -0500

    added disc numbers

---
+++ Migrations/20250212014939_AddDiscNumberToTracks.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace audiarr.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDiscNumberToTracks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DiscNumber",
+                table: "Tracks",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Albums",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscNumber",
+                table: "Tracks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Albums",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
